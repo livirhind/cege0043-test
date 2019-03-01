6abdf9495bdf78cca7e13168b03113d341280e59
livirhind <livi.rhind@gmail.com>
2019-03-01 06:21:11 -0500

    editing the string

--- uploadData.js
+++ uploadData.js
@@ -24,7 +24,7 @@ function startDataUpload(){
     //now get the geometry values
     var latitude =document.getElementById("latitude").value;
     var longitude = document.getElementById("longitude").value;
-    postString = postString + "&latitude" + latitude + "&longitude=" + longitude;
+    postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
 
 	//now get the select box values
 	var language = document.getElementById("languageselectbox").value;
