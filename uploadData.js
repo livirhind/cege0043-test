5de6c8fa8ee6fb3bf78c00dfcc1087a69cf7109c
livirhind <livi.rhind@gmail.com>
2019-03-01 05:33:26 -0500

    using an AJAX request

--- uploadData.js
+++ uploadData.js
@@ -8,3 +8,22 @@ function startDataUpload(){
 	var postString="name="+name +"&surname="+surname+"&module="+modulecode;
 	alert(postString);
 }
+
+var client; //the global variable that holds the request
+function processData(postString) {
+    client = new XMLHttpRequest() ;
+    client.open('POST', 'http://developer.cege.ucl.ac.uk:30309/reflectData',true);
+    client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    client.onreadystatechange = dataUpload;
+    //send the post string
+    client.send(postString);
+}
+
+//create the code to wait for the response from the data server, and process the response once it is received
+function dataUpload() {
+	//function listens out for the server to say that the data is ready
+	if (client.readyState == 4){
+		//change the DIV to show the response
+		document.getElementById("dataUploadResult").innerHTML= client.responseText;
+	}
+}
