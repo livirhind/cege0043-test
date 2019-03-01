560e2db552afad19d242cbc648e51739fddde21e
livirhind <livi.rhind@gmail.com>
2019-03-01 05:08:46 -0500

    taking values from the form and sending them to the server

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,10 @@
 function startDataUpload(){
 	alert("start data upload");
+	//receive the text box values
+	var name= document.getElementById("name").value;
+	var surname= document.getElementById("surname").value;
+	var modulecode= document.getElementById("module").value;
+	//put values in postring to send to the server
+	var postString="name="+name +"&surname="+surname+"&module="+modulecode;
+	alert(postString);
 }
